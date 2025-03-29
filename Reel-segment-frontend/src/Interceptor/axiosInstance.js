bdbdd2c8494be3b5896231bb74616de0b46d9509
yatharth-7022 <yathart7022@gmail.com>
2025-03-30 02:33:20 +0530

    making the interceotir

---
+++ Reel-segment-frontend/src/Interceptor/axiosInstance.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "https://your-api-url.com/",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Request Interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
